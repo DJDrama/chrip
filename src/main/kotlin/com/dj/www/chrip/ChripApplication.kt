d4d6db8c8e59b84d6d1df169646d687587bb753d
DJDrama <putzzang@naver.com>
2026-03-04 11:46:54 +0900

    Project Setup

---
+++ src/main/kotlin/com/dj/www/chrip/ChripApplication.kt
@@ -0,0 +1,11 @@
+package com.dj.www.chrip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChripApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChripApplication>(*args)
+}
---
+++ src/test/kotlin/com/dj/www/chrip/ChripApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dj.www.chrip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChripApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
