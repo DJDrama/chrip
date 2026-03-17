f0c3ce337babac8c923c238311c77bd248d9fed9
DJDrama <putzzang@naver.com>
2026-03-17 23:52:37 +0900

    46. Chat Participants Repository

---
+++ chat/src/main/kotlin/com/dj/www/chrip/infra/database/repository/ChatParticipantRepository.kt
@@ -0,0 +1,20 @@
+package com.dj.www.chrip.infra.database.repository
+
+import com.dj.www.chrip.domain.type.UserId
+import com.dj.www.chrip.infra.database.entity.ChatParticipantEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+
+interface ChatParticipantRepository : JpaRepository<ChatParticipantEntity, UserId> {
+    fun findByUserIdIn(userIds: List<UserId>): Set<ChatParticipantEntity>
+
+    @Query(
+        """
+        SELECT p
+        FROM ChatParticipantEntity p
+        WHERE LOWER(p.username) = :query OR LOWER(p.email) = :query
+    """
+    )
+    fun findByEmailOrUsername(query: String): ChatParticipantEntity?
+
+}
