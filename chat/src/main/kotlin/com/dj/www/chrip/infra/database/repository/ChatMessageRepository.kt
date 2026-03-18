d9699824c0884da1a3283e43369fed565fdd8028
DJDrama <putzzang@naver.com>
2026-03-19 00:10:31 +0900

    48. Chat Message Repository

---
+++ chat/src/main/kotlin/com/dj/www/chrip/infra/database/repository/ChatMessageRepository.kt
@@ -0,0 +1,47 @@
+package com.dj.www.chrip.infra.database.repository
+
+import com.dj.www.chrip.domain.type.ChatId
+import com.dj.www.chrip.domain.type.ChatMessageId
+import com.dj.www.chrip.infra.database.entity.ChatMessageEntity
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.time.Instant
+
+interface ChatMessageRepository : JpaRepository<ChatMessageEntity, ChatMessageId> {
+    @Query(
+        """
+        SELECT m
+        FROM ChatMessageEntity m
+        WHERE m.chatId = :chatId
+        AND m.createdAt < :before
+        ORDER BY m.createdAt DESC
+    """
+    )
+    fun findChatIdBefore(
+        chatId: ChatId,
+        before: Instant,
+        pageable: Pageable
+    ): Slice<ChatMessageEntity>
+
+    @Query(
+        """
+        SELECT m
+        FROM ChatMessageEntity m
+        LEFT JOIN FETCH m.sender
+        WHERE m.chatId IN :chatIds
+        AND (m.createdAt, m.id) = (
+            SELECT m2.createdAt, m2.id
+            FROM ChatMessageEntity m2
+            WHERE m2.chatId = m.chatId
+            ORDER BY m2.createdAt DESC
+            LIMIT 1
+        )
+    """
+    )
+    fun findLatestMessagesByChatIds(
+        chatIds: Set<ChatId>,
+    ): List<ChatMessageEntity>
+
+}
